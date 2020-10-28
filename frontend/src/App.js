22331ad437e3acb628086490592574efb880f271
David Drughi <drughidavid6@gmail.com>
2020-10-28 20:18:11 +0100

    paymentScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,8 +10,7 @@ import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
-
-
+import PaymentScreen from './screens/PaymentScreen'
 
 
 
@@ -23,12 +22,12 @@ const App = () => {
          <Container>
           <Route path='/login' component={LoginScreen}/>
           <Route path='/shipping' component={ShippingScreen}/>
+          <Route path='/payment' component={PaymentScreen}/>
           <Route path='/register' component={RegisterScreen}/>
           <Route path='/profile' component={ProfileScreen}/>
           <Route path='/product/:id' component={ProductScreen}/>
           <Route path='/cart/:id?' component={CartScreen}/>
           <Route path='/' component={HomeScreen} exact/>
-
          </Container>
          </main>
       <Footer/>
