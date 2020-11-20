924b83b839d8d3d46e77c96eda6336b03ac89eef
David Drughi <drughidavid6@gmail.com>
2020-11-20 11:25:32 +0100

    before upload to heroku

---
+++ frontend/build/precache-manifest.1f2b100445477e1023e2b0694592d44b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d2696d72bff5379f937925ad62ba18f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a6dcdc94a18ae253804",
+    "url": "/static/css/main.a370e806.chunk.css"
+  },
+  {
+    "revision": "c771c054ef3c0d9046c5",
+    "url": "/static/js/2.2e3c3e6c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2e3c3e6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a6dcdc94a18ae253804",
+    "url": "/static/js/main.70dd4146.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
